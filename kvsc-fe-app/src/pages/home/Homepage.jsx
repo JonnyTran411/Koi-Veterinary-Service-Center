3e6f4f767f2472edb94d0c24346a20526cc975d4
DatTTSE172775 <datttse172775@fpt.edu.vn>
2024-09-28 12:33:05 +0700

    Add layout veterian homepage

--- kvsc-fe-app/src/pages/home/Homepage.jsx
+++ kvsc-fe-app/src/pages/home/Homepage.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import CarouselComponent from "../../components/home/carousel/Carousel";
 import IntroductionSection from "../../components/home/introduction/Introduction";
 import ServicesOverview from "../../components/home/servicesOverviews/Services";
+import Veterian from "../../components/home/veterian/Veterian";
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
       <CarouselComponent />
       <IntroductionSection />
       <ServicesOverview />
+      <Veterian />
       {/* Thêm các component khác */}
     </>
   );
