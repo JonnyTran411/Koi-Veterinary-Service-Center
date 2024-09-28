338e6a9e80489a1c5f75886255bcf2858e47885b
DatTTSE172775 <datttse172775@fpt.edu.vn>
2024-09-28 12:23:03 +0700

    Add layout services overviews

--- kvsc-fe-app/src/pages/home/Homepage.jsx
+++ kvsc-fe-app/src/pages/home/Homepage.jsx
@@ -3,12 +3,14 @@
 import React from "react";
 import CarouselComponent from "../../components/home/carousel/Carousel";
 import IntroductionSection from "../../components/home/introduction/Introduction";
+import ServicesOverview from "../../components/home/servicesOverviews/Services";
 
 const HomePage = () => {
   return (
     <>
       <CarouselComponent />
       <IntroductionSection />
+      <ServicesOverview />
       {/* Thêm các component khác */}
     </>
   );
