1edfa35880473149b66f4bbdbbb15ce0c20e4c64
DatTTSE172775 <datttse172775@fpt.edu.vn>
2024-09-28 11:50:36 +0700

    Add enhanced carousel

--- kvsc-fe-app/src/pages/home/Homepage.jsx
+++ kvsc-fe-app/src/pages/home/Homepage.jsx
@@ -1,13 +1,18 @@
 // ./pages/home/HomePage.jsx
 
 import React from "react";
+import CarouselComponent from "../../components/carousel/Carousel";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Chào mừng đến với Koi Veterinary Service Center</h1>
-      <p>Đây là trang chủ của trung tâm dịch vụ thú y cá Koi.</p>
-    </div>
+    <>
+      <CarouselComponent />
+      <div>
+        <h1>Chào mừng đến với Koi Veterinary Service Center</h1>
+        <p>Đây là trang chủ của trung tâm dịch vụ thú y cá Koi.</p>
+      </div>
+      {/* Thêm các component khác */}
+    </>
   );
 };
 
